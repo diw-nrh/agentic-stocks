d6d5802e1bcac0660a1e2869f6d60765b4e01170
diw-nrh <fadiwmimi14@gmail.com>
2026-05-02 15:02:27 +0700

    feat: add init.sql script to enable vector extension and configure mcp_agent database role

--- db/init.sql
+++ db/init.sql
@@ -1,1 +1,19 @@
 CREATE EXTENSION IF NOT EXISTS vector;
+
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles
+      WHERE  rolname = 'mcp_agent') THEN
+
+      CREATE ROLE mcp_agent LOGIN PASSWORD 'mcp_secure_password';
+   END IF;
+END
+$do$;
+
+GRANT USAGE ON SCHEMA public TO mcp_agent;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO mcp_agent;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO mcp_agent;
